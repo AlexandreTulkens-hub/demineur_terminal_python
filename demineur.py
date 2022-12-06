1f29093bc547d168c0ad4d704b85e29cff7267fa
Alexandre Tulkens <alexandre.tulkens@ulb.be>
2022-12-06 16:36:32 +0100

    demineur.py: - ajout docstring principale   	     - ajout début de consignes d'énoncé ajout du fichier .idea

---
+++ demineur.py
@@ -0,0 +1,39 @@
+"""
+Auteur: Alexandre Tulkens
+Date: 06-12-2022
+                            Info-F-106 Projet 1 - Démineur
+ce jeux est un jeux à 1 joueur sur une grille de taille variable et dont certaines cases contiennent des mines. Le
+but du jeu est de dévoiler petit-à-petit les cases vides de la grille jusqu’à localiser toutes les mines
+et les “désamorcer”. Le joueur perd s’il dévoile une case contenant une mine.
+"""
+#import
+import sys
+
+#constantes
+LONG = sys.argv[1]  # nombre de cases en longueur du plateau
+LARG = sys.argv[2]  # nombre de cases en largeur du plateau
+NB_MINES = sys.argv[3]  # nombre de mines dans le plateau
+
+
+##################################################
+#                                                #
+#                   #fonctions                   #
+#                                                #
+##################################################
+
+def main():
+    """
+    fonction qui démarre l'éxécution du jeux
+    :return: None
+    """
+    pass
+
+
+##################################################
+#                                                #
+#                   #main                        #
+#                                                #
+##################################################
+
+if __name__ == "__main__":  # Permet a la fonction main de ce faire importer sans anomalies
+    main()
